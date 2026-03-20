7eeba53d533f59a731110f4f77f2c98c9991bdea
Md Aftab Alam <aalammdaftab4@gmail.com>
2026-03-21 00:57:19 +0530

    remove unused useCallback

--- frontend/src/pages/ProfileBuilder.jsx
+++ frontend/src/pages/ProfileBuilder.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../utils/api";
 import { useDebounce } from "../hooks/useDebounce";
